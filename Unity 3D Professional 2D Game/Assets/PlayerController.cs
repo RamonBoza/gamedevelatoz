dfe33512724689eacf9a5e921a8a83dd968fcc7d
Ramon Boza <ramon.boza@king.com>
2017-01-05 16:35:29 +0100

    Second commit adding player, box, coin and some colliders

---
+++ Unity 3D Professional 2D Game/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private bool isGrounded;
+	private float radius = 0.7f;
+	private float force = 300;
+	public Transform coinsParent;
+	public Transform groundPoint;
+	public LayerMask ground;
+	public AudioSource audioSource;
+	public AudioClip jump;
+	public AudioClip coin;
+	public AudioClip win;
+	public Animator animator;
+	public int coins;
+	// public Game gameComponent;
+	public TextMesh score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
